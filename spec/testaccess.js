ff1f353f4d5857fb52cae22b844cc9b658b07324
ganaraj <ganarajpr@gmail.com>
2014-09-21 13:12:21 +0100

    Getting sanitized expression comparison.

--- spec/testaccess.js
+++ spec/testaccess.js
@@ -58,6 +58,26 @@ function handleFirstFunction(parts){
 }
 
 
+function first(array, callback, thisArg) {
+    var n = 0,
+        length = array ? array.length : 0;
+
+    if (typeof callback != 'number' && callback != null) {
+        var index = -1;
+        callback = lodash.createCallback(callback, thisArg, 3);
+        while (++index < length && callback(array[index], index, array)) {
+            n++;
+        }
+    } else {
+        n = callback;
+        if (n == null || thisArg) {
+            return array ? array[0] : undefined;
+        }
+    }
+    return slice(array, 0, nativeMin(nativeMax(0, n), length));
+}
+
+
 function a() {
     angular.module("layout")
         .controller("OverlayController", [
@@ -136,7 +156,7 @@ function a() {
 
 }
 
-var code = handleFirstFunction.toString();
+var code = first.toString();
 
 
 var accList = acc.getAccessors(code);
